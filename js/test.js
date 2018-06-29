3fed60a6b4f00a0b0b1470728e00d3985e1e1e33
Faustin <jeminemonblog@gmail.com>
2018-06-29 18:42:42 +0200

    Relancement d'un nouveau test sur les déplacement

---
+++ js/test.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+document.getElementById("myCanvas").style.backgroundColor = 'rgba(0, 88, 255, 0.74)';
+
+//test sur les déplacement
