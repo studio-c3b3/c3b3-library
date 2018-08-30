4ab339c2054a5d90b8ad2e534e63f0fecb54a7cd
Unknown <faustino40@outlook.fr>
2018-08-30 17:47:53 +0200

    Map qui s'affiche mais en version qrcode
    - Travail sur le render
    - Le bug avec l'objet et la variable réglé (probléme avec l'asychronique)
    - Rajout de la function pour les tilesets (création optimisé avec l'object image)
    - J'oublie peut-être un truc

--- js/test.js
+++ js/test.js
@@ -3,14 +3,20 @@ var ctx = canvas.getContext("2d");
 document.getElementById("myCanvas").style.backgroundColor = 'rgba(0, 88, 255, 0.74)';
 var entity = [];
 var image = [];
-var HEIGHT = 320; // = y
-var WIDTH = 480; // = x = largeur
+var HEIGHT = 448; // = y
+var WIDTH = 640; // = x = largeur
 var gameProperty = {};
 var maps = [];
+var tileset = [];
 
 gameProperty.mapsid = 0;
 
-function createMap(id, src, tileSize) {
+function createTileset(id,src) {
+  tileset[id] = new Image();
+  tileset[id].src = src
+}
+
+function createMap(id, src, tileSize,idImage) {
   var tilesA;
   var request = new XMLHttpRequest();
   request.open('GET', src);
@@ -19,25 +25,29 @@ function createMap(id, src, tileSize) {
       var JsonRep = request.response;
       tilesA = JsonRep.tile;
       console.log(tilesA);
+      var MapO = {
+        tailleTile: tileSize,
+        tiles: tilesA,
+        idImage:idImage
+      }
+      maps[id] = MapO;
     }
   }
   request.responseType = 'json';
   request.onreadystatechange = finish;
   request.send();
 
-
-  var MapO = {
-    id:id,
-    tailleTile: tileSize,
-    tiles: tilesA
-  }
-  maps[id] = MapO;
 }
 
 function updateMaps() {
-
+  for(key in maps) {
+    for(var y = 0; y < WIDTH/maps[key].tailleTile; y++){
+      for(var x = 0; x < HEIGHT/maps[key].tailleTile; x++){
+        ctx.drawImage(tileset[maps[key].idImage], maps[key].tailleTile*maps[key].tiles[(y*HEIGHT/maps[key].tailleTile)+x], 0, maps[key].tailleTile, maps[key].tailleTile, x*maps[key].tailleTile, y*maps[key].tailleTile, maps[key].tailleTile, maps[key].tailleTile);
+    }
+  }
 }
-
+}
 
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -51,7 +61,8 @@ function update() {
 
 }
 
-createMap(0, "assets/maps/map1.json", 64)
+createTileset(0, "assets/tileset/tiletest.png")
+createMap(0, "assets/maps/map1.json", 64, 0)
 registerEntity(0, 0, 0, 10, document.getElementById("source"), 128, 128, true);
 
 registerAnimation(30, 128, 0);
