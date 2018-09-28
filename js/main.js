dabbb80a51bdccb331f585a526b54c2c48f384fe
Sakada Ly <lysakada@gmail.com>
2018-09-29 00:17:28 +0200

    ajout feature du joueur

--- js/main.js
+++ js/main.js
@@ -1,70 +1,99 @@
 let canvas = document.getElementById("myCanvas")
 let ctx = canvas.getContext("2d")
 
-const CANVAS_LARGEUR = canvas.width 
-const CANVAS_HAUTEUR = canvas.height
+let imageMer = new Image();
+imageMer.src = 'assets/images/mer.png'; 
 
-let imageMer = new Image()
-imageMer.src = 'assets/img/mer.png' 
-
-let imageBateauHero = new Image()
-imageBateauHero.src = 'assets/img/bateau_hero.png'
-
+let imageBateauHero = new Image();
+imageBateauHero.src = 'assets/images/bateau_hero.png';
 
 let bateauHero = {
-    position : {
-        x: 250,
-        y: 250
-    },
-    vitesse : {
-        x:-1,
-        y:-1
-    },
-    angle : 0.5,
-}
-
-
-console.log(CANVAS_LARGEUR/imageMer.width)
-console.log(CANVAS_HAUTEUR/imageMer.height)
-
+  position : {
+    x: 250,
+    y: 250,
+  },
+  vitesse : 0, 
+  angle : 0,
+  rotation : 'still',
+};
 
 function tapisserCanvas (imageParam) {
-    for (var i = 0; i < CANVAS_LARGEUR/imageParam.width; i++) {
-        for (var j = 0; j < CANVAS_HAUTEUR/imageParam.height; j++) {
-            console.log(imageParam.width);
-            ctx.drawImage(imageParam, i * imageParam.width, j * imageParam.height);
-        }
+  for (var i = 0; i < canvas.width/imageParam.width; i++) {
+    for (var j = 0; j < canvas.height/imageParam.height; j++) {  
+      ctx.drawImage(imageParam, i * imageParam.width, j * imageParam.height);
     }
+  }
 }
 
 function updatePosition(acteur) {
-    acteur.position.x += acteur.vitesse.x;
-    acteur.position.y += acteur.vitesse.y;
+  acteur.position.x += acteur.vitesse * Math.sin(convertToRadian(acteur.angle));
+  acteur.position.y += acteur.vitesse * -Math.cos(convertToRadian(acteur.angle));
+  if (acteur.rotation == 'gauche') {
+    acteur.angle -= 2;
+  } else if (acteur.rotation == 'droite') {
+    acteur.angle += 2; 
+  } 
+
 }
 
 function update() {
-    tapisserCanvas(imageMer);
-    ctx.save();
-    ctx.drawImage(imageBateauHero, bateauHero.position.x, bateauHero.position.y);
-    ctx.restore();
-    console.log("bateau position x : " + bateauHero.position.x);
-    console.log("bateau position y : " + bateauHero.position.y);
-    updatePosition(bateauHero);
+  tapisserCanvas(imageMer);
+  rotateEntity(imageBateauHero,bateauHero.position.x, bateauHero.position.y, bateauHero.angle);
+  updatePosition(bateauHero);
+}
+
+function convertToRadian(degrees) {
+  return degrees * Math.PI/180;
+}
+
+function rotateEntity(image, positionX, positionY, angle) {
+  ctx.save();
+  ctx.translate(positionX+image.width/2, positionY);
+  ctx.rotate(convertToRadian(angle));
+  ctx.drawImage(image, -image.width/2, -image.height/2);
+  ctx.restore();
+
 }
 
 imageMer.onload = function(){
-    tapisserCanvas(imageMer);
+  tapisserCanvas(imageMer);
 }
-
-
 
 imageBateauHero.onload = function() {
-    ctx.save();
-    ctx.translate(imageBateauHero.width/2, imageBateauHero.height/2 )
-    radians = (Math.PI/180)*90;
-    ctx.rotate(radians);
-    ctx.drawImage(imageBateauHero, bateauHero.position.x, bateauHero.position.y);
-    ctx.restore();
+  rotateEntity(imageBateauHero,bateauHero.position.x, bateauHero.position.y, bateauHero.angle);
 }
 
-setInterval(update, 40)
+window.onload = function(){
+  setInterval(update, 40);
+}
+
+
+/************ CONTROLE ************/
+document.onkeydown = function(event){
+  if(event.keyCode === 68) { //d
+    bateauHero.rotation = 'droite';
+  }
+
+	else if(event.keyCode === 83) {//s
+    bateauHero.vitesse  = 0;
+  }
+
+	else if(event.keyCode === 81) {//q
+    bateauHero.rotation = 'gauche';
+  }
+
+	else if(event.keyCode === 90){// z 
+    bateauHero.vitesse  = -2;
+  }
+}
+
+document.onkeyup = function(event){
+  if(event.keyCode === 68) { //d
+    bateauHero.rotation = 'still';
+  }
+
+	else if(event.keyCode === 81) {//q
+    bateauHero.rotation = 'still';
+  }
+
+}
