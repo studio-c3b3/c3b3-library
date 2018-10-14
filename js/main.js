ccabe07ef15cfd3b98b16f7b65cc257d2a8e3b92
Faustin <faustino40@outlook.fr>
2018-10-14 11:21:00 +0200

    On avance sur le menu !
    - Afficahge du menu fonctionnel
    - Réparation du callback
    - Ajout s'un assets

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext("2d");
 var gamePropriete = {};
 var menuElement = [];
 gamePropriete.menuCursor = 0;
-gamePropriete.manuCursorId = 0;
+
 gamePropriete.mapsReader = "xy";
 gamePropriete.mapsid = 0;
 gamePropriete.player = 0;
@@ -35,16 +35,16 @@ function updateMenu() {
 function declarerMenuCursor(id,width,height,srcImage,zoom,zoomFacteur){
   declarerEntite(id, 0, 0, 0, srcImage, width, height, 2);
   declarerStatic(id, width, height, zoom, zoomFacteur);
-  menuCursorId = 0;
+  gamePropriete.menuCursorId = id;
   entite[id].imageRendu.cursor = true;
 }
 
 function declarerMenuElement(id,x,width,height,srcImage,zoom,zoomFacteur,callback) {
-  declarerEntite(id, x, WIDTH/2-width/2, 0, srcImage, width, height, 2);
+  declarerEntite(id, WIDTH/2-width/2, x, 0, srcImage, width, height, 2);
   declarerStatic(id, width, height, zoom, zoomFacteur);
   entite[id].imageRendu.active = false;
   entite[id].imageRendu.cursor = false;
-  entite[id].callback() = callback;
+  entite[id].callback = function(){callback();}
   menuElement.push(id);
 }
 
@@ -62,9 +62,9 @@ function update() {
     }
   }
 }
-declarerMenuCursor(3, 64, 64, "asstes/img/cursor.png", 0, 0)
-declarerMenuElement(1, 0, 96, 40, "assets/img/play.png", 0, 0, console.log("test"))
-declarerMenuElement(2, 180, 96, 40, "assets/img/play.png", 0, 0, console.log("test 1"))
+declarerMenuCursor(3, 22, 21, "assets/img/menu/arrowBlue_right.png", 1, 1)
+declarerMenuElement(1, 0, 96, 40, "assets/img/menu/play.png", 1, 1, function(){console.log("test")});
+declarerMenuElement(2, 180, 96, 40, "assets/img/menu/play.png", 1, 1, function(){console.log("test 1")})
 declarerMap(0, "assets/maps/map1.json");
 declarerEntite(0, 0, 0, 10, "assets/img/bateauPirates.png", 128, 128, 0);
 declarerStatic(0, 128, 128, 0, 0)
