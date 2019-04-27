f06d169392f2c1b01119cec8468e748efa36d132
Leith <lchakroun@outlook.fr>
2019-04-27 15:05:53 +0200

    Voilà
    J'ai enlevé un play parce que ça sert à rien d'en avoir  deux et j'ai mis le bateau en dessous en guise de logo ( je ferai un logo plus tard) et on peut pas le sélectionner avec le curseur.

--- js/class/menu.js
+++ js/class/menu.js
@@ -30,15 +30,11 @@ function declarerMenuElement(id,x,width,height,srcImage,zoom,zoomFacteur,callbac
     menuElement.push(id);
 }
 
-function declarerMenuElement(id,x,width,height,srcImage,zoom,zoomFacteur,callback) {
-    declarerEntite(id, WIDTH/2-width/2, x, 0, srcImage, width, height, 2);
-    declarerStatic(id, width, height, zoom, zoomFacteur);
-    entite[id].imageRendu.active = false;
-    entite[id].imageRendu.cursor = false;
-    entite[id].callback = () => callback();
-    menuElement.push(id);
-}
+function declarerMenuLogo(id,x,width,height,srcImage,zoom,zoomFacteur,callback) {
+  declarerEntite(id, WIDTH/2-width/2, x, 0, srcImage, width, height, 2);
+  declarerStatic(id, width, height, zoom, zoomFacteur);
 
+}
 
 function ajouteProprieteMenu(id,nom,valeur){
     if(!etats[id]) console.error("L'état n'existe pas !");
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const HEIGHT = 448;
 const WIDTH = 640;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-let gamePropriete = {};
+var gamePropriete = {};
 
 gamePropriete.menuCursor = 0; //emplacement du curseur
 gamePropriete.menuCursorId = null; // Le curseur actif
@@ -14,10 +14,16 @@ gamePropriete.etat = 0;
 gamePropriete.mainMenu = 0;
 gamePropriete.playerInput = "rotate";
 
+function rotateEntity(id, angle) {
+    ctx.save();
+    ctx.translate(entite[id].x+entite[id].width/2, entite[id].y);
+    ctx.rotate(gameMath.convertToRadian(angle));
+    entite[id].imageRendu.gen();
+    ctx.restore();
+}
 
 function update() {
     etats[gamePropriete.etat].update();
-    setTimeout(update, 10);
 }
 
 declarerEtat(0, 0,
@@ -51,34 +57,30 @@ declarerEtat(1, 1,
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         updateMaps();
         updateJoueurPosition();
-        drawImage();});
+        if(gamePropriete.playerInput !== "rotate") drawImage();});
 
 declarerAudio(0, "./assets/song/defilement.ogg");
 declarerAudio(1, "./assets/song/clic.ogg");
 declarerAudio(2, "./assets/song/boucleMenu.mp3");
 declarerMenuCursor(3, 22, 21, "assets/img/menu/arrowBlue_right.png", 1, 1);
 declarerMenuElement(1, 0, 96, 40, "assets/img/menu/play.png", 1, 1, () => {changeEtat(1); audio[1].play()});
-declarerMenuElement(2, 180, 96, 40, "assets/img/menu/play.png", 1, 1, () => {console.log("test 1"); audio[1].play();});
-declarerMenuElement(3, 260, 96, 40, "assets/img/menu/logo.png");
+//declarerMenuElement(2, 180, 96, 40, "assets/img/menu/play.png", 1, 1, () => {console.log("test 1"); audio[1].play();});
+declarerMenuLogo(2, 300, 96, 40, "assets/img/bateau_hero.png", 1, 1, );
 declarerMap(0, "assets/maps/map1.json");
-declarerEntite(0, 0, 73, 0, "assets/img/bateau_hero.png", 146, 146, 0);
-//declarerEntite(1, 0, 0, 0, "assets/img/bateau_hero.png", 146, 146, 0);
+declarerEntite(0, 0, 0, 10, "assets/img/bateau_hero.png", 146, 146, 0);
 declarerStatic(0, 146,146,1,1);
+setInterval(update, 10);
 ajouteProprieteMenu(0, "sonDefilement",audio[0]);
 changeEtat(0);
 
-if(gamePropriete.playerInput === "rotate") entite[gamePropriete.player].imageRendu.gen = function() {
-    ctx.save();
-    ctx.translate(entite[gamePropriete.player].x+entite[gamePropriete.player].width/2, entite[gamePropriete.player].y);
-    ctx.fillRect(0, 0, 10, 10);
-    ctx.rotate(gameMath.convertToRadian(entite[gamePropriete.joueur].angle));
+if(gamePropriete.playerInput === "rotate") entite[gamePropriete.player].image.gen = function() {
+    ctx.beginPath();
     if(entite[gamePropriete.player].image.zoom){
-        ctx.drawImage(entite[gamePropriete.player].image, 0, 0, entite[gamePropriete.player].imageRendu.width, entite[gamePropriete.player].imageRendu.height, -entite[gamePropriete.player].width/2, -entite[gamePropriete.player].height/2, (entite[gamePropriete.player].imageRendu.width*entite[gamePropriete.player].imageRendu.zoomFacteur), (entite[gamePropriete.player].imageRendu.height*entite[gamePropriete.player].imageRendu.zoomFacteur));
+        ctx.drawImage(entite[gamePropriete.player].image.image, 0, 0, entite[gamePropriete.player].image.width, entite[gamePropriete.player].image.height, entite[id].x, entite[id].y, -(entite[gamePropriete.player].image.width*entite[gamePropriete.player].image.zoomFacteur), -(entite[gamePropriete.player].image.height*entite[gamePropriete.player].image.zoomFacteur));
     }
     else{
-        ctx.drawImage(entite[gamePropriete.player].image, 0, 0, entite[gamePropriete.player].imageRendu.width, entite[gamePropriete.player].imageRendu.height, -entite[gamePropriete.player].width/2, -entite[gamePropriete.player].height/2, (entite[gamePropriete.player].imageRendu.width/entite[gamePropriete.player].imageRendu.zoomFacteur), (entite[gamePropriete.player].imageRendu.height/entite[gamePropriete.player].imageRendu.zoomFacteur));
+        ctx.drawImage(entite[gamePropriete.player].image.image, 0, 0, entite[gamePropriete.player].image.width, entite[gamePropriete.player].image.height, entite[id].x, entite[id].y, -(entite[gamePropriete.player].image.width/entite[gamePropriete.player].image.zoomFacteur), -(entite[gamePropriete.player].image.height/entite[gamePropriete.player].image.zoomFacteur));
     }
-    ctx.restore();
-};
+    ctx.closePath();
 
-setTimeout(update, 1000);
+};
